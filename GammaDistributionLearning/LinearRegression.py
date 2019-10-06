1e0539c15aec2e3f61083972e19f754883a056b0
adamlu123 <yadongl1@ics.uci.edu>
2019-10-06 16:06:47 -0700

    sampler and linearmodel class

--- GammaDistributionLearning/LinearRegression.py
+++ GammaDistributionLearning/LinearRegression.py
@@ -7,27 +7,75 @@ import numpy as np
 import MarsagliaTsampler
 
 
-def generate_data(n, p, phi, seed):
+def generate_data(n, p, phi, rho, seed):
     np.random.seed(seed)
+    noise = np.random.normal(loc=0, scale=phi, size=[n, p])  # noise, with std phi
+
+    sigma = rho * np.ones((p, p))
+    np.fill_diagonal(sigma, 1)
+    X = np.random.multivariate_normal(mean=np.zeros(p), cov=sigma, size=2)  # X, with correlation rho
+
+    truetheta = np.asarray([0.6, 1.2, 1.8, 2.4, 3])
+    beta = np.zeros(p)
+    beta[-5:] = truetheta  # beta
+
+    Y = X * beta + noise
+    return Y, X, truetheta
 
 
-    return Y, X, truebeta, selected_id
+class SpikeAndSlabSampler(nn.module):
+    def __init__(self, p, alternative_sampler):
+        self.p = p
+        self.logalpha = nn.Parameter(torch.ones(p))
+        self.alternative_sampler = alternative_sampler(size=p)
+
+        # L0 related parameters
+        self.zeta = 1.1
+        self.gamma = -0.1
+        self.beta = 2 / 3
+
+    def forward(self, batch_size):
+        # sample theta
+        theta = self.alternative_sampler(batch_size=batch_size) # shape=[batch_size, p]
+
+        # sample z
+        u = Uniform(0, 1).sample([batch_size,self.p])
+        s = torch.sigmoid((torch.log(u / (1 - u)) + self.logalpha) / self.beta)
+        z = torch.clamp((self.zeta - self.gamma) * s + self.gamma, 0, 1)
+
+        if not self.training:
+            z = torch.clamp(torch.sigmoid(self.logalpha) * (self.zeta - self.gamma) + self.gamma, 0, 1)
+
+        return z * theta
 
 
 class LinearModel(nn.module):
     def __init__(self, p):
-        self.sampler = MarsagliaTsampler(size=p)
+        self.sampler = SpikeAndSlabSampler(size=p, alternative_sampler=MarsagliaTsampler)
+
     def forward(self, x):
-        theta = self.sampler(batch_size=32).mean(axis=0)
+        theta = self.sampler(batch_size=32).mean(axis=0)  # TODO: defer taking mean to the output
         out = x * theta
         return out
 
+    def loglike(self, y_hat, y):
+        std = y.std()
+        return torch.log(1/(torch.sqrt(2*np.pi)*std)) -(y_hat-y)**2/(2*std**2)
 
-def elbo(y_hat, Y):
+    def kl(self, ):
 
-    return likelihood + kl
 
-def train(Y, X, truebeta, selected_id, epoch=10000):
+    def elbo(self, y_hat, y):
+        return loglike(y_hat, y) - kl
+
+
+# def elbo(y_hat, y):
+#     std = y.std()
+#     loglike = torch.log(1/(torch.sqrt(2*np.pi)*std)) -(y_hat-y)**2/(2*std**2)
+#     kl =
+#     return likelihood + kl
+
+def train(Y, X, truetheta, epoch=10000):
     linear = LinearModel(p=X.shape[0])
 
 
@@ -35,7 +83,7 @@ def train(Y, X, truebeta, selected_id, epoch=10000):
 
     for i in range(epoch):
         y_hat = linear(X)
-        loss = -elbo(y_hat, Y)
+        loss = -linear.elbo(y_hat, Y)
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
@@ -54,5 +102,7 @@ def main():
     n = 100
     for p in [100, 500, 1000]:
         for phi in [1, 4, 8]:
-            Y, X, truebeta, selected_id = generate_data(n, p, phi, seed=1234)
-            train(Y, X, truebeta, selected_id, epoch=10000)
+            Y, X, truetheta = generate_data(n, p, phi, rho=0, seed=1234)
+            train(Y, X, truetheta, epoch=10000)
+
+
