fix data shape and kl

NeuReparam/beta-gamma-MF.py
@@ -1,3 +1,5 @@
+
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -5,8 +7,8 @@ import torch.utils.data
 from torch.autograd import Function
 import pickle as pkl
 from torch.distributions import Gamma, Beta
-import torch.distributions.kl.kl_divergence as kl_divergence
-
+from torch.distributions.kl import kl_divergence
+import numpy as np
 
 hp = {
     'data_folder': '/extra/yadongl10/data/MNIST/',
@@ -47,6 +49,8 @@ class StochasticBetaLayer(Function):
 
     @staticmethod
     def backward(ctx, output_grad):
+        import pydevd
+        pydevd.settrace(suspend=False, trace_only_current_thread=True)
         z, theta, eta = ctx.saved_tensors
         updated_z = z + output_grad * 1
         updated_z = updated_z.detach()
@@ -61,32 +65,32 @@ class BetaGammaMF(nn.Module):
         # self.prior = prior
         self.StochasticGammaLayer = StochasticGammaLayer
         self.StochasticBetaLayer = StochasticBetaLayer
-        self.logalpha = nn.Parameter(torch.ones(100, 1024))
-        self.logbeta = nn.Parameter(torch.ones(100, 1024))
-        self.logtheta = nn.Parameter(torch.ones(500, 100))
-        self.logeta = nn.Parameter(torch.ones(500, 100))
+        self.logalpha = nn.Parameter(torch.ones(100, 784))
+        self.logbeta = nn.Parameter(torch.ones(100, 784))
+        self.logtheta = nn.Parameter(torch.ones(5000, 100))
+        self.logeta = nn.Parameter(torch.ones(5000, 100))
 
     def forward(self, x):
         alpha, beta, theta, eta = self.logalpha.exp(), self.logbeta.exp(), \
                                   self.logtheta.exp(), self.logeta.exp()
-        w = self.StochasticGammaLayer(alpha, beta)
-        z = self.StochasticBetaLayer(theta, eta)
+        w = self.StochasticGammaLayer.apply(alpha, beta)
+        z = self.StochasticBetaLayer.apply(theta, eta)
         logit_z = torch.log(z / (1 - z))
         out = torch.sigmoid(logit_z.matmul(w))
         return out
 
     def get_kl(self):
         gamma_q = Gamma(concentration=self.logalpha.exp(), rate=self.logbeta.exp())
-        gamma_p = Gamma(0.1, 0.3)
+        gamma_p = Gamma(0.1*torch.ones_like(self.logalpha), 0.3*torch.ones_like(self.logalpha))
         beta_q = Beta(self.logtheta.exp(), self.logeta.exp())
-        beta_p = Beta(1, 1)
+        beta_p = Beta(torch.ones_like(self.logtheta), torch.ones_like(self.logtheta))
         # kl = _kl_beta_beta(beta_q, beta_p) + _kl_gamma_gamma(gamma_q, gamma_p)
-        kl = kl_divergence(beta_q, beta_p) + kl_divergence(gamma_q, gamma_p)
+        kl = kl_divergence(beta_q, beta_p).sum() + kl_divergence(gamma_q, gamma_p).sum()
         return kl
 
     def get_elbo(self, out, data):
-        ll = data * out.log() + (1 - data) * (1 - out).log()
-        elbo = ll - self.get_kl()
+        ll = data * out.log() + (1 - data) * (1 - out + 1e-10).log()
+        elbo = ll.sum() - self.get_kl()
         return elbo
 
 
@@ -94,10 +98,11 @@ def train(model, optimizer, train_loader):
     model.train()
     elbo_list = []
     for batch_idx, data in enumerate(train_loader):
+        data = data[0]
         optimizer.zero_grad()
 
         out = model(data)
-        elbo = model.get_elbo(out)
+        elbo = model.get_elbo(out, data)
         loss = - elbo
         loss.backward()
         optimizer.step()
@@ -107,32 +112,29 @@ def train(model, optimizer, train_loader):
             print(elbo.tolist())
 
 
-# def test(model, test_loader):
-
-
 def prepare_data(subset, hp):
     if subset == 'train':
-        with open(hp['data_folder'] + 'binarized_mnist_train.amat', 'rb') as f:
-            data = pkl.load(f)
-            data = data[:5000, :, :]
-            print(data.shape)
+        data = np.loadtxt(hp['data_folder'] + 'binarized_mnist_train_small.txt')
+        # data = data[:5000, :]
+        print(data.shape)
     elif subset == 'test':
-        with open(hp['data_folder'] + 'binarized_mnist_test.amat', 'rb') as f:
-            data = pkl.load(f)
-            data = data[:1000, :, :]
-            print(data.shape)
+        data = np.loadtxt(hp['data_folder'] + 'binarized_mnist_test.amat')
+        data = data[:1000, :]
+        print(data.shape)
 
     data = torch.tensor(data, dtype=torch.float32).to(hp['device'])
     data = torch.utils.data.TensorDataset(data)
-    data_loader = torch.utils.data.DataLoader(data, batch_size=32, shuffle=True)
+    data_loader = torch.utils.data.DataLoader(data, batch_size=5000, shuffle=True)
     return data_loader
 
 
 def main(hp):
     # Define model
-    model = BetaGammaMF(StochasticGammaLayer, StochasticBetaLayer)
+    print('start define model')
+    model = BetaGammaMF(StochasticGammaLayer, StochasticBetaLayer).to(hp['device'])
 
     # prepare data
+    print('start loading data')
     train_loader = prepare_data('train', hp)
     test_loader = prepare_data('test', hp)
 
@@ -140,7 +142,11 @@ def main(hp):
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     # training
+    print('start training')
     for epoch in range(hp['epochs']):
         print('Epoch: ', epoch)
         train(model, optimizer, train_loader)
         # test(model, test_loader)
+
+if __name__ == '__main__':
+    main(hp)