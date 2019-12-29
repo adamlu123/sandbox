4990d65e0cade4034d781e9b1c5aaf97ff0c7e05
adamlu123 <yadongl1@ics.uci.edu>
2019-12-28 16:37:37 -0800

    MVP lognormm linear regression

--- NonLocalPrior/LinearRegressionLognorm.py
+++ NonLocalPrior/LinearRegressionLognorm.py
@@ -24,18 +24,34 @@ def generate_data(n, p, phi, rho, seed):
     return Y, X, theta
 
 
+def lognorm_logpdf(theta, mu, logstd):
+    theta = theta.abs()
+    logstd = torch.ones_like(logstd)
+    return - (theta.log() + logstd + (torch.tensor(2*np.pi)).sqrt().log()) - (theta.log() - mu)**2/(2*(logstd.exp()**2)).clamp(min=1e-1, max=1e1)
+
+
+def get_qlogq(theta, mu, logstd, weight):
+    """
+    neg entropy of the variational distribution: qlogq, 1/S*logprob(samples)
+    :param theta:
+    :return:
+    """
+    qlogq = torch.where(theta > 0,
+                        weight * lognorm_logpdf(theta, mu, logstd),
+                        (1 - weight) * lognorm_logpdf(-theta, mu, logstd))
+    return qlogq
 
 
 class LogNormalSampler(nn.Module):
     def __init__(self, p):
         super(LogNormalSampler, self).__init__()
         self.mu = nn.Parameter(.2*torch.ones(p))
-        self.logstd = nn.Parameter(.2*torch.ones(p))
+        self.logstd = nn.Parameter(1*torch.ones(p))
         self.p = p
 
     def forward(self, repeat):
         epsilon = Normal(0, 1).sample([repeat, self.p])
-        return (self.logstd.exp() * epsilon + self.mu).exp()
+        return (self.logstd.exp() * epsilon + self.mu).exp(), self.mu, self.logstd
 
 
 class HardConcreteSampler(nn.Module):
@@ -50,13 +66,14 @@ class HardConcreteSampler(nn.Module):
         self.logalpha = nn.Parameter(torch.ones(p))
 
     def forward(self, repeat):
+        qz = torch.sigmoid(self.logalpha - self.beta * self.gamma_zeta_logratio)
         u = Uniform(0, 1).sample([repeat, self.p])
         s = torch.sigmoid((torch.log(u / (1 - u)) + self.logalpha) / self.beta)
         if self.training:
             z = torch.clamp((self.zeta - self.gamma) * s + self.gamma, 0, 1)
         else:
             z = torch.clamp(torch.sigmoid(self.logalpha) * (self.zeta - self.gamma) + self.gamma, 0, 1).expand_as(u)
-        return z
+        return z, qz
 
 
 class ConcreteSampler(nn.Module):
@@ -89,17 +106,17 @@ class SpikeAndSlabSampler(nn.Module):
         self.mixture_weight_sampler = ConcreteSampler(p)
 
     def forward(self, repeat):
-        theta = self.alternative_sampler(repeat)
+        theta, mu, logstd = self.alternative_sampler(repeat)
         weight = self.mixture_weight_sampler(repeat)
-        z = self.z_sampler(repeat)
-        out = z * (weight * theta + (1 - weight) * theta)
-        return out
+        z, qz = self.z_sampler(repeat)
+        signed_theta = weight * theta + (1 - weight) * (-theta)
+        out = z * signed_theta
+        return out, signed_theta, mu, logstd, weight, z, qz
 
 
 class LinearModel(nn.Module):
     """
     Wrap around SpikeAndSlabSampler for a linear regression model, use Gamma distribution as variational distribution
-
     compare with: https://www.tandfonline.com/doi/pdf/10.1080/01621459.2015.1130634?needAccess=true
     """
     def __init__(self, p, bias=False, alternative_sampler=LogNormalSampler):
@@ -113,29 +130,101 @@ class LinearModel(nn.Module):
         self.sampler = SpikeAndSlabSampler(p=p, alternative_sampler=alternative_sampler)
 
     def forward(self, x):
-        self.Theta = self.sampler(repeat=60)
-        out = x.matmul(self.Theta.mean(dim=0).view(x.shape[1], -1)) + self.bias  # TODO: defer taking mean to the output
-        return out
+        self.Theta, self.signed_theta, self.mu, self.logstd, self.weight, self.z, self.qz = self.sampler(repeat=100)
+        out = x.matmul(self.Theta.mean(dim=0).view(x.shape[1], -1))  # TODO: defer taking mean to the output
+        return out.squeeze()
 
     def kl(self, phi):
         p = 5e-3
-        qz = self.z_mean.expand_as(self.theta)
+        qz = self.qz.expand_as(self.Theta)
         kl_z = qz * torch.log(qz / p) + (1 - qz) * torch.log((1 - qz) / (1 - p))
-        qlogp = torch.log(nlp_pdf(self.signed_theta, phi, tau=0.358)+1e-8)
-
-        if isinstance(self.alternative_sampler, MarsagliaTsampler):
-            gamma = Gamma(concentration=self.detached_gamma_alpha, rate=1.)
-            qlogq = np.log(0.5) + gamma.log_prob(self.theta)  # use unsigned self.theta to compute qlogq
-        elif isinstance(self.alternative_sampler, LogNormalSampler):
-            qlogq = self.log_prob_alternative(self.signed_theta)
+        qlogp = torch.log(nlp_pdf(self.signed_theta, phi, tau=0.358)+1e-8)   # shape=(60, p)
+        qlogq = get_qlogq(self.signed_theta, self.mu, self.logstd, self.weight)
 
         kl_beta = qlogq - qlogp
         kl = (kl_z + qz*kl_beta).sum(dim=1).mean()
-        return kl, kl_z, kl_beta
+        return kl, qlogp.mean(), qlogq.mean()
 
-    def log_prob_alternative(self, theta):
-        """
-        neg entropy of the variational distribution: qlogq, 1/S*logprob(samples)
-        :param theta:
-        :return:
-        """
+
+
+def loglike(y_hat, y):
+    ll = - (y_hat-y)**2/(2*1**2)
+    return ll.sum()
+
+def train(Y, X, truetheta, phi, epoch=10000):
+    Y = torch.tensor(Y, dtype=torch.float)
+    X = torch.tensor(X, dtype=torch.float)
+    linear = LinearModel(p=X.shape[1])
+    optimizer = optim.SGD(linear.parameters(), lr=0.0001, momentum=0.9)
+    sse_list = []
+    sse_theta_list = []
+    for i in range(epoch):
+        linear.train()
+        optimizer.zero_grad()
+
+        # forward pass and compute loss
+        y_hat = linear(X)
+        nll = -loglike(y_hat, Y)
+        kl, qlogp, qlogq = linear.kl(phi)
+        loss = nll + kl
+        # print('qlogp, qlogq', qlogp.data, qlogq.data)
+
+        # compute gradient and do SGD step
+        loss.backward()
+        optimizer.step()
+
+        sse = ((y_hat - Y) ** 2).mean().detach().numpy()
+        sse_list.append(sse)
+
+        with torch.no_grad():
+            linear.eval()
+            y_hat = linear(X)
+            z = linear.z
+            sse_theta = ((linear.Theta.mean(dim=0) - torch.tensor(truetheta, dtype=torch.float)) ** 2).sum()
+            sse_test = ((y_hat - Y) ** 2).mean().detach().numpy()
+            sse_theta_list.append(((linear.Theta.mean(dim=0) - torch.tensor(truetheta, dtype=torch.float)) ** 2).sum())
+
+        # print intermediet results
+        if i % 200 == 0:
+            z = linear.z
+            print('\n', 'last 5 responses:', y_hat[-5:].round().tolist(), Y[-5:].round().tolist())
+            print('sse_theta:{}, min_sse_theta:{}'.format(sse_theta, np.asarray(sse_theta_list).min()))
+            print('est.Thetas: {}, est z:{}'.format(linear.Theta.mean(dim=0)[-5:].tolist(), z.mean(dim=0).detach().numpy()))
+            print('epoch {}, z min: {}, z mean: {}, non-zero: {}'.format(i, z.min(), z.mean(), z.nonzero().shape))
+            # print('p={}, phi={}, loss: {}, nll:{}, kl:{}. SSE: {}, sse_test: {}'.format(X.shape[0], phi, nll, loss, kl, sse, sse_test))
+    plt.plot(sse_list)
+    # plt.savefig('/extra/yadongl10/git_project/GammaLearningResult/sse.png', dpi=100)
+    return linear
+
+
+def test(Y, X, model):
+    model.eval()
+    y_hat = model(X)
+    sse = ((y_hat - Y) ** 2).mean().detach().numpy()
+
+    print('test SSE:{}'.format(sse))
+    if model.z.nonzero().shape[0] < 10:
+        print(model.z.nonzero(), model.z[-5:].tolist())
+
+
+config = {
+    'save_model': False,
+    'save_model_dir': '/extra/yadongl10/git_project/nlpresult'
+}
+
+
+def main(config):
+    n = 100
+    for p in [10]:
+        for phi in [1, 4, 8]:
+            Y, X, truetheta = generate_data(n, p, phi, rho=0, seed=1234)
+            linear = train(Y, X, truetheta, phi, epoch=10000)
+            test(Y, X, linear)
+
+            # if config['save_model']:
+            #     torch.save(linear.state_dict(), config['save_model_dir']+'lognorm_nlp_linear_p1000.pt')
+
+
+if __name__=='__main__':
+    main(config)
+
