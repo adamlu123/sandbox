7d40281d90d119edb9d68279ed2d5f658889143a
adamlu123 <yadongl1@ics.uci.edu>
2020-01-06 12:40:55 -0800

    add SigmoidFlow

--- NonLocalPrior/LinearRegressionFlow.py
+++ NonLocalPrior/LinearRegressionFlow.py
@@ -1,9 +1,16 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from utils import log
 from torch import optim
 from torch.distributions import Normal, Uniform, Gamma, Bernoulli
 import matplotlib.pyplot as plt
 import numpy as np
+from torch.autograd import Variable
+import utils
+
+def logit(x):
+    return torch.log(x/(1-x))
 
 
 def nlp_pdf(x, phi, tau=0.358):
@@ -12,6 +19,10 @@ def nlp_pdf(x, phi, tau=0.358):
 
 def nlp_log_pdf(x, phi, tau=0.358):
     return (x**2).clamp(min=1e-2).log() - np.log(tau) - np.log(np.sqrt(2*np.pi*tau*phi)) - x**2/(2*tau*phi)
+
+
+def normal_log_pdf(x):
+
 
 
 def generate_data(n, p, phi, rho, seed):
@@ -52,21 +63,134 @@ class HardConcreteSampler(nn.Module):
         return z, qz
 
 
+class BaseFlow(nn.Module):
+    def sample(self, n=1, context=None, **kwargs):
+        dim = self.dim
+        if isinstance(self.dim, int):
+            dim = [dim, ]
+
+        spl = Variable(torch.FloatTensor(n, *dim).normal_())
+        lgd = Variable(torch.from_numpy(
+            np.zeros(n).astype('float32')))
+        if context is None:
+            context = Variable(torch.from_numpy(
+                np.ones((n, self.context_dim)).astype('float32')))
+
+        if hasattr(self, 'gpu'):
+            if self.gpu:
+                spl = spl.cuda()
+                lgd = lgd.cuda()
+                context = context.gpu()
+
+        return self.forward((spl, lgd, context))
+
+    def cuda(self):
+        self.gpu = True
+        return super(BaseFlow, self).cuda()
+
+
+class SigmoidFlow(BaseFlow):
+
+    def __init__(self, num_ds_dim=4):
+        super(SigmoidFlow, self).__init__()
+        self.num_ds_dim = num_ds_dim
+
+        self.act_a = lambda x: utils.softplus(x)
+        self.act_b = lambda x: x
+        self.act_w = lambda x: utils.softmax(x, dim=2)
+
+    def forward(self, x, logdet, dsparams, mollify=0.0, delta=utils.delta):
+        ndim = self.num_ds_dim
+        a_ = self.act_a(dsparams[:, :, 0 * ndim:1 * ndim])
+        b_ = self.act_b(dsparams[:, :, 1 * ndim:2 * ndim])
+        w = self.act_w(dsparams[:, :, 2 * ndim:3 * ndim])
+
+        a = a_ * (1 - mollify) + 1.0 * mollify
+        b = b_ * (1 - mollify) + 0.0 * mollify
+
+        pre_sigm = a * x[:, :, None] + b
+        sigm = torch.sigmoid(pre_sigm)
+        x_pre = torch.sum(w * sigm, dim=2)
+        x_pre_clipped = x_pre * (1 - delta) + delta * 0.5
+        x_ = log(x_pre_clipped) - log(1 - x_pre_clipped)
+        xnew = x_
+
+        logj = F.log_softmax(dsparams[:, :, 2 * ndim:3 * ndim], dim=2) + \
+               utils.logsigmoid(pre_sigm) + \
+               utils.logsigmoid(-pre_sigm) + log(a)
+
+        logj = utils.log_sum_exp(logj, 2).sum(2)
+        logdet_ = logj + np.log(1 - delta) - (log(x_pre_clipped) + log(-x_pre_clipped + 1))
+        logdet = logdet_.sum(1) + logdet
+
+        return xnew, logdet
+
+
 class FlowAlternative(nn.Module):
     """
-    Independent component-wise flow
+    Independent component-wise flow using SigmoidFlow
     """
-    def __init__(self, p):
+    def __init__(self, p, num_ds_dim=4, num_ds_layers=1):
         super(FlowAlternative, self).__init__()
         self.p = p
+        self.num_ds_dim = num_ds_dim
+        self.num_ds_layers = num_ds_layers
+        self.dsparams =  # TODO: set learnable parameters
 
-    def forward(self, repeat):
-        epsilon = Normal(0, 1).sample([repeat, self.p])
-        return epsilon
+        self.sf = SigmoidFlow(num_ds_dim)
+
+    def forward(self, inputs):
+        nparams = self.num_ds_dim * 3
+        x, logdet, context = inputs
+
+        h = inputs.view(inputs.size(0), -1)   # TODO: ?
+        for i in range(self.num_ds_layers):
+            params = self.dsparams[:, :, i * nparams:(i + 1) * nparams]   # shape=(repeat, p, nparams)
+            h, logdet = self.sf(h, logdet, params, mollify=0.0)
+
+        return h, logdet
+
+
+# class LocalLinear(nn.Module):
+#     def __init__(self,in_features,local_features,kernel_size,padding=0,stride=1,bias=True,softmax_weight=False):
+#         super(LocalLinear, self).__init__()
+#         self.kernel_size = kernel_size
+#         self.stride = stride
+#         self.padding = padding
+#
+#         fold_num = (in_features+2*padding-self.kernel_size)//self.stride+1
+#         self.weight = nn.Parameter(torch.randn(fold_num,kernel_size,local_features))
+#         if softmax_weight:
+#             self.weight = F.softmax(self.weight)   # TODO: check which dim should apply softmax
+#         self.bias = nn.Parameter(torch.randn(fold_num,local_features)) if bias else None
+#
+#     def forward(self, x):
+#         x = F.pad(x,[self.padding]*2,value=0)
+#         x = x.unfold(-1,size=self.kernel_size,step=self.stride)
+#         x = torch.matmul(x.unsqueeze(2),self.weight).squeeze(2)+self.bias
+#         return x
+#
+#
+# class FlowAlternative(nn.Module):
+#     """
+#     Independent component-wise flow
+#     """
+#     def __init__(self, p):
+#         super(FlowAlternative, self).__init__()
+#         self.p = p
+#         self.LocalLinear_in = LocalLinear(p, local_features=5, kernel_size=1)
+#         self.LocalLinear_out = LocalLinear(p, local_features=1, kernel_size=1)
+#
+#     def forward(self, repeat):
+#         epsilon = Normal(0, 1).sample([repeat, self.p])
+#         x = torch.sigmoid(self.LocalLinear_in(epsilon))  # shape=(d,p)
+#         out = logit(self.LocalLinear_out(x))
+#         return out
+
 
 class SpikeAndSlabSampler(nn.Module):
     """
-    Add spike and slab to LogNormalSampler
+    Spike and slab sampler with Dirac spike and FlowAlternative slab.
     """
     def __init__(self, p, alternative_sampler=FlowAlternative):
         super(SpikeAndSlabSampler, self).__init__()
@@ -75,15 +199,15 @@ class SpikeAndSlabSampler(nn.Module):
         self.z_sampler = HardConcreteSampler(p)
 
     def forward(self, repeat):
-        theta, epsilon, mu, logstd = self.alternative_sampler(repeat)
+        theta, logdet = self.alternative_sampler.sample(n=repeat)
         z, qz = self.z_sampler(repeat)
         out = z * theta
-        return out, epsilon, mu, logstd, z, qz
+        return out, logdet, z, qz
 
 
 class LinearModel(nn.Module):
     """
-    Wrap around SpikeAndSlabSampler for a linear regression model, use Gamma distribution as variational distribution
+    Wrap around SpikeAndSlabSampler for a linear regression model.
     compare with: https://www.tandfonline.com/doi/pdf/10.1080/01621459.2015.1130634?needAccess=true
     """
     def __init__(self, p, bias=False, alternative_sampler=FlowAlternative):
@@ -97,7 +221,7 @@ class LinearModel(nn.Module):
         self.sampler = SpikeAndSlabSampler(p=p, alternative_sampler=alternative_sampler)
 
     def forward(self, x):
-        self.Theta, self.signed_theta, self.epsilon, self.mu, self.logstd, self.weight, self.z, self.qz = self.sampler(repeat=100)
+        self.Theta, self.logdet, self.z, self.qz = self.sampler(repeat=64)
         out = x.matmul(self.Theta.mean(dim=0).view(x.shape[1], -1))  # TODO: defer taking mean to the output
         return out.squeeze()
 
@@ -105,9 +229,8 @@ class LinearModel(nn.Module):
         p = 1e-3
         qz = self.qz.expand_as(self.Theta)
         kl_z = qz * torch.log(qz / p) + (1 - qz) * torch.log((1 - qz) / (1 - p))
-        # qlogp = torch.log(nlp_pdf(self.signed_theta, phi, tau=0.358)+1e-5)   # shape=(60, p)
-        qlogp = nlp_log_pdf(self.signed_theta, phi, tau=0.358).clamp(min=np.log(1e-10))
-        qlogq = get_qlogq_robust(self.signed_theta, self.epsilon, self.mu, self.logstd, self.weight)
+        qlogp = nlp_log_pdf(self.Theta, phi, tau=0.358).clamp(min=np.log(1e-10))
+        qlogq = self.logdet
 
         kl_beta = qlogq - qlogp
         kl = (kl_z + qz*kl_beta).sum(dim=1).mean()
